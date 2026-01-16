dd8b379cb5e2342195c0508f64a0a3669b961a95
everythingisurban <apilkcphd@gmail.com>
2026-01-16 18:18:24 -0500

    Fix import path after renaming alexanderplatz to Nepalslope

--- data/gallery/index.ts
+++ data/gallery/index.ts
@@ -2,7 +2,7 @@
 import { GalleryItem } from '../../types';
 import { data as shibuya } from './shibuya';
 import { data as barbican } from './barbican';
-import { data as alexanderplatz } from './alexanderplatz';
+import { data as alexanderplatz } from './Nepalslope';
 import { data as marinaCity } from './marina-city';
 import { data as highLine } from './high-line';
 import { data as kowloon } from './kowloon';
